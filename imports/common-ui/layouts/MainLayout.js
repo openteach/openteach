986e6edfd30b3ca11d6e4d09715aa8395621f2e4
Joachim Harris <joachimharris@gmail.com>
2016-10-24 13:19:36 +0200

    Added common-ui folder in imports

---
+++ imports/common-ui/layouts/MainLayout.js
@@ -0,0 +1,18 @@
+// import React, { PropTypes } from 'react';
+// // import { Provider } from 'react-redux';
+// // import store from '/imports/redux/store';
+//
+// const MainLayout = ({ content }) => {
+//   // return <Provider store={store}>
+//   //   {content()}
+//   // </Provider>
+//   return <div>
+//     {content()}
+//   </div>
+// };
+//
+// MainLayout.propTypes = {
+//   content: PropTypes.func.isRequired,
+// };
+//
+// export default MainLayout
