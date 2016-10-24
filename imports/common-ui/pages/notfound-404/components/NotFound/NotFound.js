0e5b89bee8018d8cc48f60f707fca6566f6d7ae7
Joachim Harris <joachimharris@gmail.com>
2016-10-24 13:57:50 +0200

    Added structure for notfound-404 component

---
+++ imports/common-ui/pages/notfound-404/components/NotFound/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <div className="notFound">
+                <h1 className="notFound__headline">{this.props.headline}</h1>
+            </div>
+        )
+    }
+}
+
+NotFound.propTypes = {
+    headline: React.propTypes.string.isRequired
+}
+
+NotFound.defaultProps = {
+    headline: 'Ohh... 404 = not found'
+}
