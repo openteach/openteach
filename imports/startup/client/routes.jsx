8ae86d98aa68a26858a06ab7722629e501b6e085
Mads Buch <madspbuch@gmail.com>
2016-11-13 20:01:50 +0100

    merge contiued. Functional state

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import PublicLayout from '../../common-ui/layouts/PublicLayout.jsx';
-import AppLayout from '../../common-ui/layouts/AppLayout.jsx';
 
 import Entrypage from '../../common-ui/pages/entrypage/Entrypage.jsx';
 import NotFound from '../../common-ui/pages/notfound-404/components/NotFound/NotFound.js';
@@ -35,62 +34,6 @@ publicGroup.route('/', {
     action(params, queryParams){
         mount(PublicLayout, {
             content: (<Entrypage />)
-        })
-    }
-});
-
-/**
- * User routes
- */
-
-// /u/course/:courseId...
-userGroup.route('/course/:id/:chapterId?', {
-    name: "bookRoute",
-    action(params, queryParams) {
-        mount(AppLayout, {
-            content: (
-                <Dashboard selected={1}>
-                    <Book id={params.id} chapterId={params.chapterId} />
-                </Dashboard>)
-        });
-    }
-});
-
-userGroup.route('/dashboard/apprenticeship', {
-    name: "dashboardApprenticeship",
-    action(params, queryParams) {
-        mount(AppLayout, {
-            content: (<Dashboard selected={0} />)
-        })
-    }
-});
-
-userGroup.route('/dashboard/books', {
-    name: "dashboardBooks",
-    action(params, queryParams) {
-        mount(AppLayout, {
-            content: (
-                <Dashboard selected={1}>
-                    <ListBooks />
-                </Dashboard>)
-        })
-    }
-});
-
-userGroup.route('/dashboard/lectures', {
-    name: "dashboardLectures",
-    action(params, queryParams) {
-        mount(AppLayout, {
-            content: (<Dashboard selected={2} />)
-        })
-    }
-});
-
-userGroup.route('/apprenticeship', {
-    name: "apprenticeshipRoute",
-    action(params, queryParams) {
-        mount(AppLayout, {
-            content: (<Apprenticeship />)
         })
     }
 });
