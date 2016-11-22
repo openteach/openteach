b8e120e1a8244967862fe4e1a0268e0cf5b45b76
Joachim Harris <joachimharris@Joachims-MacBook-Pro.local>
2016-11-22 12:37:23 +0100

    Fixed merge conflict in navigation component

--- imports/common-ui/pages/entrypage/Navigation.jsx
+++ imports/common-ui/pages/entrypage/Navigation.jsx
@@ -8,20 +8,10 @@ export default class Navigation extends React.Component {
 
                     <h5 className="textLogo float-left">openTeach</h5>
 
-<<<<<<< HEAD
-					<a
-						href="https://github.com/openteach/openTeach">
-							<i
-								className="whiteText githubLogo fa fa-github fa-2x float-right"
-								aria-hidden="true">
-							</i>
-					</a>
-=======
                     <a href="https://github.com/openteach/openTeach" className="githubLogo float-right whiteText">
                         <i className="fi-social-github"></i>
                     </a>
->>>>>>> 3ffade8f01bd37195a181b10775e09029ba37d02
-
+                    
                 </div>
             </div>
         )
