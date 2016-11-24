b3d68992cd9166cfd16777592cfa774be76b373d
Joachim Harris <joachimharris@Joachims-MacBook-Pro.local>
2016-11-24 16:17:41 +0100

    Updated small changes for Navigation

--- imports/common-ui/pages/entrypage/Navigation.jsx
+++ imports/common-ui/pages/entrypage/Navigation.jsx
@@ -9,9 +9,9 @@ export default class Navigation extends React.Component {
                     <h5 className="textLogo float-left">openTeach</h5>
 
                     <a href="https://github.com/openteach/openTeach" className="githubLogo float-right whiteText">
-                        <i className="fi-social-github"></i>
+                        <i className="githubLogo__text fi-social-github">GitHub</i>
                     </a>
-                    
+
                 </div>
             </div>
         )
