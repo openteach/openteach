3b280bfec62b6fce0c11e4a75cf808a789e46331
Mads Buch <madspbuch@gmail.com>
2016-12-08 18:33:52 +0100

    added error message

--- imports/common-ui/pages/EntryPage/SignUpInput.jsx
+++ imports/common-ui/pages/EntryPage/SignUpInput.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export default class SignUpInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error : false,
+            errorReason : ""
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+
     onSubmit(e){
         e.preventDefault();
-
+        let that = this;
         //TODO: Create meteor user
         Accounts.createUser({
             profile : {
@@ -11,12 +22,12 @@ export default class SignUpInput extends React.Component {
             },
             email : e.target.umail.value,
             password : e.target.upass.value
-        }, function(e){
-            if(typeof e === 'undefined'){
+        }, function(err){
+            if(typeof err === 'undefined'){
                 FlowRouter.go("dashboardApprenticeship");
             }else{
-                //login error
-                console.log("Error signing up");
+                that.setState({error : true, errorReason : err.reason})
+                console.log(err);
             }
         });
 
@@ -27,6 +38,13 @@ export default class SignUpInput extends React.Component {
         return (
             <div className="signUpInput text-center">
                 <form onSubmit={this.onSubmit}>
+                    <div className={this.state.error ? "" : "hide"}>
+                        <div className="medium-centered medium-10 columns">
+                            <div className="callout alert">
+                                {this.state.errorReason}
+                            </div>
+                        </div>
+                    </div>
                     <input name="uname" type="text" placeholder="Name" ref={(f) => {this._uname = f}} />
                     <input name="umail" type="text" placeholder="Email" ref={(f) => this._umail = f} />
                     <input name="upass" type="text" placeholder="Password" ref={(f) => this._upass = f} />
