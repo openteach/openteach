0d4ea7292957657e5b5db1e4941f81968debe3c4
Mads Buch <madspbuch@gmail.com>
2016-12-10 15:44:52 +0100

    alternative login

---
+++ imports/common-ui/pages/EntryPage/UnvAccounts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ComboBox} from 'meteor/universe:accounts-ui';
+
+export default class UnvAccounts extends React.Component {
+    render() {
+        return ( <ComboBox /> )
+    }
+};
