f2a937665823a265594c2e7b62173c8c772d06cb
Mads Buch <madspbuch@gmail.com>
2017-01-11 20:17:22 +0100

    topic cards testing added

--- imports/features/apprenticeship/components/TopicCard/TopicCardContainer.js
+++ imports/features/apprenticeship/components/TopicCard/TopicCardContainer.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
-import TopicCard from './TopicCard.js';
+import TopicCard from './TopicCard.jsx';
 
 export default createContainer((params) => {
 
