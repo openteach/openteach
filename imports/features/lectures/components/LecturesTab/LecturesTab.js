ef2cb05b33bc663799f64cb5439d1527ab8d7cfd
Joachim Harris <joachimharris@Joachims-MacBook-Pro.local>
2017-01-18 22:21:12 +0100

    added header

--- imports/features/lectures/components/LecturesTab/LecturesTab.js
+++ imports/features/lectures/components/LecturesTab/LecturesTab.js
@@ -4,24 +4,37 @@ import Radium from 'radium';
 class LecturesTab extends Component {
     render() {
         return (<div className="lectures__back">
-                    <div>
-                        <h5 className="text-center">
-                            The lecture section is coming later
-                        </h5>
-                    </div>
-                    <div className="row text-center">
-                        <input
-                            type="submit"
-                            className="button success"
-                            value="I want lectures"
-                        />
-                    </div>
-                </div>);
+            <div className="expanded row text-center" style={styles.header}>
+                <div className="large-12 columns">
+                    <h1>Teachers lectures</h1>
+                    <p style={styles.tagline}>Teachers public lectures</p>
+                </div>
+            </div>
+            <div className="row text-center">
+                <input
+                    type="submit"
+                    className="button success"
+                    value="I want lectures"
+                />
+            </div>
+        </div>);
   }
 }
 
 LecturesTab.propTypes = {};
 
 LecturesTab.defaultProps = {};
+
+const styles = {
+    header: {
+        'paddingTop': '3em',
+        'height': '12.5em',
+        'backgroundColor': '#ecf0f1',
+        'marginBottom': '4em'
+    },
+    tagline: {
+        'fontSize': '1.2em'
+    }
+}
 
 export default Radium(LecturesTab)
