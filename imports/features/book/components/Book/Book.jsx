a6be290e7417ef0ee494bbeae4714a3f78eacd25
Joachim Harris <joachimharristech@gmail.com>
2017-02-16 22:03:20 +0100

    Added Offcanvas menu

--- imports/features/book/components/Book/Book.jsx
+++ imports/features/book/components/Book/Book.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Remarkable from 'remarkable';
 import Meta from 'remarkable-meta';
 import Radium from 'radium';
+import { OffCanvas, OffCanvasMenu, OffCanvasBody } from 'react-offcanvas';
 
 class Book extends Component {
     renderChapterContent(){
@@ -26,7 +27,7 @@ class Book extends Component {
         return (
             <div>
                 <div className="row">
-                    <div className="large-12 columns" style={styles}>{chapter.meta.title}</div>
+                    <div className="large-12 columns" style={styles.headline}>{chapter.meta.title}</div>
                 </div>
                 <div className="row">
                     <div>{chapter.meta.tags}</div>
@@ -56,24 +57,45 @@ class Book extends Component {
             );
         });
     }
+
+    componentWillMount() {
+    // sets the initial state
+        this.setState({
+            isMenuOpened: false
+        })
+    }
+
+    handleClick() {
+      // toggles the menu opened state
+      this.setState({ isMenuOpened: !this.state.isMenuOpened });
+    }
+
     render() {
         if(this.props.loading)
             return (<div>Book is loading</div>)
         else
-            return  (<div id="chapter-selector" className="row expanded">
-                        <div className="chapterList small-2 large-2 columns chapterList__ul">
-                            <p className="list__logo__text">
-                                OpenTeach
-                            </p>
-                            <span className="list__logo"></span>
-                            <ul>
-                                {this.renderChapterList()}
-                            </ul>
-                        </div>
-                        <div className="small-10 large-10 columns">
-                            {this.renderChapterContent()}
-                        </div>
-                    </div>);
+            return  (
+                <div id="chapter-selector" className="row expanded">
+                    <OffCanvas width={250} transitionDuration={250} isMenuOpened={this.state.isMenuOpened} position={"left"}>
+                        <OffCanvasBody className={styles.bodyClass}>
+                            <div className="small-12 large-12 columns">
+                                <a><i onClick={this.handleClick.bind(this)} className="fi-indent-more" style={styles.toggleIcon}></i></a>
+                                {this.renderChapterContent()}
+                            </div>
+                        </OffCanvasBody>
+                        <OffCanvasMenu className={styles.menuClass}>
+                            <div className="chapterList small-12 large-12 columns chapterList__ul">
+                                <p className="list__logo__text" style={styles.menuLogo}>OpenTeach</p>
+                                <ul style={styles.listMenu}>
+                                    {this.renderChapterList()}
+                                    <li><a onClick={this.handleClick.bind(this)}>Close menu</a></li>
+                                </ul>
+                            </div>
+                        </OffCanvasMenu>
+                    </OffCanvas>
+
+                </div>
+            );
     }
 }
 
@@ -87,8 +109,25 @@ Book.propTypes = {
 Book.defaultProps = {};
 
 const styles = {
-    'fontSize': '3rem',
-    'fontWeight': 'bold'
+    headline: {
+        'fontSize': '3rem',
+        'fontWeight': 'bold',
+    },
+    bodyClass: {
+
+    },
+    toggleIcon: {
+        'fontSize': '1.5em'
+    },
+    menuClass: {
+        
+    },
+    menuLogo: {
+        'paddingTop': '4em'
+    },
+    listMenu: {
+        'listStyle': 'none'
+    }
 }
 
 export default Radium(Book)
